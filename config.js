077b56873075877473eeb4dcb6d33dbabddc7eb0
KIPSIGEI7 <167616063+KIPSIGEI7@users.noreply.github.com>
2024-04-20 14:07:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['+254799054988', 'knight bot', true],
+  ['254799054988', 'knight bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
